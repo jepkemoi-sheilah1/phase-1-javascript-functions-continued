48200a90246b925c1f1344c391017980e43afa29
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-06 22:08:02 +0300

    add function to complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){//    function that accepts an activity
+    return `This Saturday, I want to ${activity}!`//return string  
+}
+function mondayWork(activity="go to the office"){//    function that accepts an activity
+    return `This Monday, I will ${activity}.`//return string
+}
+function wrapAdjective(flair="*"){
+    return function(adjective="special"){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
